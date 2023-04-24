4f8e260362e69eb1ebbc8e8faa869860a6b21c42
SKR <sascha@krusbersky.net>
2023-04-24 14:01:11 +0200

    Modify CI/CD

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
+import App from './App'
 
-import { BannerFull } from './components/BannerFull/index.tsx'
+//import { BannerFull } from './components/BannerFull/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BannerFull />
+    <App />
   </React.StrictMode>,
 )
