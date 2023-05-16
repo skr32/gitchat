68e63a8a7804ea971a0bd591e085b5423d1f886a
Clemens HuberL <clemens.huber00@gmail.com>
2023-05-16 14:56:42 +0200

    improve link

--- src/components/Join/index.tsx
+++ src/components/Join/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-// import {Link} from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import './style.scss'
 import logo from '../../assets/chatLogo.png'
 import { SvgButton } from '../SvgButton';
@@ -20,9 +20,11 @@ export function Join() {
                 <input placeholder="Room ID" type="text"
                     onChange={(event) => setRoom(event.target.value)}
                 ></input>
-                {/* <Link onClick={(event)=>(!name || !room)?event.preventDefault():null} to={`/chat?name=${name}&room=${room}`}> */}
-                <SvgButton text={'Join Chat'} link={''} />
-                {/* </Link> */}
+                <BrowserRouter>
+                <Link onClick={ (event) => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <SvgButton text={'Join Chat'} />
+                </Link>
+                </BrowserRouter>
             </div>
         </div>
     )
--- src/components/SvgButton/index.tsx
+++ src/components/SvgButton/index.tsx
@@ -1,13 +1,13 @@
 import './style.scss'
 
-export function SvgButton(info :{text:string, link: string}) {
-    return(
+export function SvgButton(info: { text: string }) {
+    return (
         <button className="svgbutton glow-effect">
-            <a href={info.link}>{info.text}</a>
-                <svg className="glow-container">
-                    <rect className="glow-container__blur" pathLength="100" strokeLinecap="round"></rect>
-                    <rect className="glow-container__line" pathLength="100" strokeLinecap="round"></rect>
-                </svg>
+            {info.text}
+            <svg className="glow-container">
+                <rect className="glow-container__blur" pathLength="100" strokeLinecap="round"></rect>
+                <rect className="glow-container__line" pathLength="100" strokeLinecap="round"></rect>
+            </svg>
         </button>
     )
 }
