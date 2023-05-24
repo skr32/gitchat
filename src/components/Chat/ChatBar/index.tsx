fd5b596ba8d4b456cc6adea451c38d9c658717f7
Clemens HuberL <clemens.huber00@gmail.com>
2023-05-24 22:09:09 +0200

    fix sidebar

--- src/components/Chat/ChatBar/index.tsx
+++ src/components/Chat/ChatBar/index.tsx
@@ -5,46 +5,50 @@ import { ChatPreview } from './ChatPreview';
 
 export function ChatBar() {
     const [isOpen, setIsOpen] = useState(true);
-    
-      const toggleMenu = () => {
+
+    const toggleMenu = () => {
         setIsOpen(!isOpen);
-      };
+    };
 
     return (
         <>
             <button className="sidebar__burger"
                 aria-expanded={isOpen}
                 onClick={toggleMenu}
-                style={{ position: isOpen ? 'absolute' : 'fixed', left: !isOpen ? '25px' : '210px'}}>
+                style={{
+                    position: isOpen ? 'absolute' : 'fixed',
+                    left: !isOpen ? '25px' : '250px'
+                }}>
                 <svg viewBox="0 0 100 100">
                     <rect className="line top" rx="5"></rect>
                     <rect className="line middle" rx="5"></rect>
                     <rect className="line bottom" rx="5"></rect>
                 </svg>
             </button>
-        <div className='sidebar-container' style={{visibility: !isOpen ? 'hidden' : 'visible', width: isOpen ? '300px' : '80px',}}>
-            <div className='sidebar' style={{
-                minWidth: isOpen ? '300px' : '0',
+            <div className='sidebar-container' style={{
                 visibility: !isOpen ? 'hidden' : 'visible',
-                transform: !isOpen ? 'translateX(-100%)' : 'translateX(0%)',
-                transition: 'all 0.5s ease',
+                minWidth: isOpen ? '300px' : '0',
+                width: isOpen ? '0' : '80px',
             }}>
-                <div>
+                <div className='sidebar' style={{
+                    minWidth: isOpen ? '300px' : '0',
+                    transform: !isOpen ? 'translateX(-500px)' : 'translateX(0)',
+                    transition: 'all 0.5s ease',
+                }}>
+                    <div>
 
-                <span className='sidebar__title'>
-                <h2>CodeChat<img src={logo} alt="logo" /></h2>
-                </span>
-                <div className='sidebar__upper'>
-                    <span></span>
-                    <h2>Inbox</h2>
-                    <input type="text" placeholder='Search Chats...'></input>
-                    <span></span>
+                        <span className='sidebar__title'>
+                            <h2>CodeChat<img src={logo} alt="logo" /></h2>
+                        </span>
+                        <div className='sidebar__upper'>
+                            <h2>Inbox</h2>
+                            <input type="text" placeholder='Search Chats...'></input>
+                        </div>
+                    </div>
+                    <div className='sidebar__preview'>
+                        <ChatPreview />
+                    </div>
                 </div>
-                </div>
-                <div className='sidebar__preview'>
-                    <ChatPreview />
-                </div>
-            </div>
             </div>
         </>
     )
