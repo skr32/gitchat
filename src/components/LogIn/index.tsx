b6499a75561c680af3a1f5af9c6aa33a4ed322f3
Clemens HuberL <clemens.huber00@gmail.com>
2023-05-28 22:32:55 +0200

    add signup and animation to login

--- src/components/LogIn/index.tsx
+++ src/components/LogIn/index.tsx
@@ -9,11 +9,25 @@ export function LogIn() {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [login, setLogin] = useState(false);
+    const [signUp, setSignUp] = useState(true);
+
+    const toggle = () => {
+        if (login === true) {
+            setLogin(false);
+            setSignUp(true);
+        } else if (signUp === true) {
+            setLogin(true);
+            setSignUp(false);
+        }
+    }
 
     return (
+        <>
         <div className="join-container">
+  
             <div className="join-container__inner">
-                <h2>CodeChat<img src={logo} alt="logo" /></h2>
+                <h2>Log In<img src={logo} alt="logo" /></h2>
                 <input placeholder="Username" type="text"
                     onChange={(event) => setName(event.target.value)}
                 ></input>
@@ -21,11 +35,48 @@ export function LogIn() {
                     onChange={(event) => setRoom(event.target.value)}
                 ></input>
                 <BrowserRouter>
-                <Link onClick={ (event) => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <SvgButton text={'Log In'} />
-                </Link>
+                    <Link onClick={(event) => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                        <SvgButton text={'Log In'} />
+                    </Link>
+                </BrowserRouter>
+            </div>
+            <div className="join-container__inner">
+                <h2><img src={logo} alt="logo" /> Sign Up</h2>
+                <input placeholder="Username" type="text"
+                    onChange={(event) => setName(event.target.value)}
+                ></input>
+                <input placeholder="Password" type="password"
+                    onChange={(event) => setRoom(event.target.value)}
+                ></input>
+                <BrowserRouter>
+                    <Link onClick={(event) => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                        <SvgButton text={'Sign Up'} />
+                    </Link>
                 </BrowserRouter>
             </div>
         </div>
+        <div className='overlay_container'>
+                <div className="join-container__inner overlay"
+                    style={{
+                        transform: !login ? 'translateX(100%)' : 'translateX(0)',
+                        opacity: !login ? '1' : '0',
+                        zIndex: !login ? '1' : '0',
+                    }}>
+                    <h2>Welcome back</h2>
+                    <p>Don't have an acoount?</p><br />
+                    <button onClick={toggle}>Sign up</button>
+                </div>
+                <div className="join-container__inner overlay" style={{
+                    transform: !signUp ? 'translateX(-100%)' : 'translateX(0)',
+                    opacity: !login ? '0' : '1',
+                    zIndex: !login ? '0' : '1',
+                }}>
+                    <h2>Create Account</h2>
+                    <p>Already a member?</p> <br />
+                    <button onClick={toggle}>LogIn</button>
+
+                </div>
+            </div>
+        </>
     )
 }
