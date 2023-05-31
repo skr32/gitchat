637e11fddbcb128d8f49b75c16b33c1d95473633
nicolekum <nicolekumar393@yahoo.de>
2023-05-31 14:25:04 +0200

    create timestamp

--- src/components/Chat/Chat/Message/index.tsx
+++ src/components/Chat/Chat/Message/index.tsx
@@ -1,32 +1,32 @@
 import './style.scss'
 let messages = [
-   {me:true, text:'lkjlklkasdflkdsajfldsjföalskfjakfjakdsjfasfdsaj'},
-   {me:true, text:'lkjleorituiokj'},
-   {me:false, text:'dsfdsKAÖDLFSJHKGDDFGHEIRWUHGUEIf'},
-   {me:true, text:'lkjlyxcvkj'},
-   {me:false, text:'dsfdsf'},
-   {me:true, text:'lkjlafdsakj'},
-   {me:true, text:'lkjbncvlkj'},
-   {me:false, text:'dsfasdfdsf'},
-   {me:true, text:'lkjlkj'},
-   {me:false, text:'dsfhgdsf'},
-   {me:true, text:'lkjlasdfkj'},
-   {me:true, text:'lkjlkj'},
-   {me:false, text:'dsfsdfjhdsf'},
-   {me:true, text:'lkjlhgjkj'},
-   {me:false, text:'dsf4fghdsf'},
+   {me:true, text:'lkjlklkasdflkdsajfldsjföalskfjakfjakdsjfasfdsaj', timestamp: '10:30'},
+   {me:true, text:'lkjleorituiokj', timestamp: '10:35'},
+   {me:false, text:'dsfdsKAÖDLFSJHKGDDFGHEIRWUHGUEIf', timestamp: '10:45'},
+   {me:true, text:'lkjlyxcvkj', timestamp: '10:55'},
+   {me:false, text:'dsfdsf', timestamp: '10:56'},
+   {me:true, text:'lkjlafdsakj', timestamp: '10:57'},
+   {me:true, text:'lkjbncvlkj', timestamp: '10:58'},
+   {me:false, text:'dsfasdfdsf',timestamp: '10:59'},
+   {me:true, text:'lkjlkj',timestamp: '11:02'},
+   {me:false, text:'dsfhgdsf',timestamp: '11:04'},
+   {me:true, text:'lkjlasdfkj',timestamp: '11:05'},
+   {me:true, text:'lkjlkj',timestamp: '11:07'},
+   {me:false, text:'dsfsdfjhdsf',timestamp: '11:08'},
+   {me:true, text:'lkjlhgjkj',timestamp: '11:10'},
+   {me:false, text:'dsf4fghdsf',timestamp: '11:12'},
 ]
 export function Message() {
-    return(
-        <>
+    return (
+      <>
         {messages.map((message) => (
-            <> 
-            <div className={`message ${message.me ? 'right-corner' : 'left-corner'}`} key={message.text}>
-                {message.text}
+          <div className={`message ${message.me ? 'right-corner' : 'left-corner'}`} key={message.text}>
+            <div className="message-text">{message.text}</div>
+            <div className={`message-timestamp ${message.me ? 'left-message' : 'right-message'}`}>
+              {message.timestamp}
             </div>
-            </>
-        )
-            )}
-        </>
-    )
-}
+          </div>
+        ))}
+      </>
+    );
+  }
