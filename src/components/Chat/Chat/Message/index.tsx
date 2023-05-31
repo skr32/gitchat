4991e4b6ddc74b8b0278577895f5d89dc7333cfb
Clemens HuberL <clemens.huber00@gmail.com>
2023-05-31 22:35:48 +0200

    add messages from database

--- src/components/Chat/Chat/Message/index.tsx
+++ src/components/Chat/Chat/Message/index.tsx
@@ -1,67 +1,73 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './style.scss'
-let messages = [
-   {me:true, text:'lkjlklkasdflkdsajfldsjföalskfjakfjakdsjfasfdsaj'},
-   {me:true, text:'lkjleorituiokj'},
-   {me:false, text:'dsfdsKAÖDLFSJHKGDDFGHEIRWUHGUEIf'},
-   {me:true, text:'lkjlyxcvkj'},
-   {me:false, text:'dsfdsf'},
-   {me:true, text:'lkjlafdsakj'},
-   {me:true, text:'lkjbncvlkj'},
-   {me:false, text:'dsfasdfdsf'},
-   {me:true, text:'lkjlkj'},
-   {me:false, text:'dsfhgdsf'},
-   {me:true, text:'lkjlasdfkj'},
-   {me:true, text:'lkjlkj'},
-   {me:false, text:'dsfsdfjhdsf'},
-   {me:true, text:'lkjlhgjkj'},
-   {me:false, text:'dsf4fghdsf'},
-]
+
+let returnedThreadId: any = null;
+export function renderMessages(threadId:any){
+    returnedThreadId = threadId;
+        return(threadId);
+}
 export function Message() {
-    const [chats, setChats] = useState<{ name: string; message: string }[]>([]);
+    const [processedMessages, setProcessedMessages] = useState<{ name: string, message: string, date: Date }[]>([]);
+    // const token = localStorage.getItem('token');
+    const token = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzcyYzk0MmM0MDhjNGI5MzRmYWU1OSIsIm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjg1NTMxODEzLCJleHAiOjE3MTcwODg3Mzl9.mCre5BG5C5psHn7Eqe3W1F3IPSLam2os8Jrv7B66HDo';
+    function parseJwt(token:string) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    }
+    const decodedToken = parseJwt(token);
+    console.log(decodedToken.name);
 
     useEffect(() => {
-      const token = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzcyYzk0MmM0MDhjNGI5MzRmYWU1OSIsIm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjg1NTMxODEzLCJleHAiOjE3MTcwODg3Mzl9.mCre5BG5C5psHn7Eqe3W1F3IPSLam2os8Jrv7B66HDo';
-      const threadId = e.target.getAttribute('data-thread-id');
-      fetch('http://localhost:5000/api/messages/allmessages?thread=' + threadId, {
+        console.log(returnedThreadId);
+      }, [returnedThreadId]);
+      setTimeout(() => console.log(returnedThreadId), 3000)
+    useEffect(() => {
+        // to do: make threadId dynamic
+    //   const token = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzcyYzk0MmM0MDhjNGI5MzRmYWU1OSIsIm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjg1NTMxODEzLCJleHAiOjE3MTcwODg3Mzl9.mCre5BG5C5psHn7Eqe3W1F3IPSLam2os8Jrv7B66HDo';
+    //   const token = localStorage.getItem('token');
+    //   fetch('http://localhost:5000/api/messages/allmessages?thread=' + returnedThreadId, {
+      fetch('http://localhost:5000/api/messages/allmessages?thread=64772ee52c408c4b934fae68', {
         headers: {
             'Authorization': `${token}`
         }
     })
         .then(response => response.json())
         .then(messages => {
-            messages.forEach(message => addMessage(message))
-        })
-        // .then(() => {
-        //     // Scroll to bottom of message list
-        //     messagesList.scrollTop = messagesList.scrollHeight;
-        // })
-        .catch(error => console.error(error));
-
-    // // Prevent form submission and send message to server on button click
-    // const form = document.querySelector('#chat-window form');
-    // form.addEventListener('submit', e => {
-    //     e.preventDefault();
-    //     const input = document.getElementById('message-input');
-    //     const message = input.value;
-    //     input.value = '';
-
-    //     if (message === '') {
-    //         return;
-    //     }
-
-    }, []);
+            const newMessages = messages.map(message => {
+              const isCurrentUser = decodedToken.name === message.fromUsername;
+              return {
+                name: isCurrentUser ? 'true' : 'false',
+                message: message.message,
+                date: message.date
+              };
+            });
     
-    return(
+            setProcessedMessages(prevMessages => [...prevMessages, ...newMessages]);
+          })
+          .catch(error => {
+            console.error('Error fetching messages:', error);
+          });
+      }, []);
+
+    return (
         <>
-        {messages.map((message) => (
-            <> 
-            <div className={`message ${message.me ? 'right-corner' : 'left-corner'}`} key={message.text}>
-                {message.text}
-            </div>
-            </>
-        )
-            )}
+        {processedMessages.length > 0 ? (
+        processedMessages.map((message, index) => (
+          <div
+          className={`message ${message.name === 'true' ? 'right-corner' : 'left-corner'}`}
+            key={index}
+          >
+            {message.message}
+          </div>
+        ))
+      ) : (
+        <div>Loading messages...</div>
+      )}
         </>
     )
 }
--- src/components/Chat/ChatBar/ChatPreview/index.tsx
+++ src/components/Chat/ChatBar/ChatPreview/index.tsx
@@ -1,47 +1,51 @@
 import './style.scss';
 import logo from '../../../../assets/chatLogo.png';
 import { useEffect, useState } from 'react';
+import { renderMessages } from '../../Chat/Message';
 
+export let selectedThreadId: string = '';
 export function ChatPreview() {
+    const [selectedThreadId, setSelectedThreadId] = useState('');
+
+    renderMessages(selectedThreadId);
+
     const [loading, setLoading] = useState(true);
-  const [chats, setChats] = useState<{ name: string; message: string }[]>([]);
+    const [chats, setChats] = useState<{ name: string; message: string }[]>([]);
 
-  useEffect(() => {
-    const token = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzcyYzk0MmM0MDhjNGI5MzRmYWU1OSIsIm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjg1NTMxODEzLCJleHAiOjE3MTcwODg3Mzl9.mCre5BG5C5psHn7Eqe3W1F3IPSLam2os8Jrv7B66HDo';
-    fetch('http://localhost:5000/api/threads/allthreads', {
-      headers: {
-        'Authorization': `${token}`
-      }
-    })
-      .then(response => response.json())
-      .then(threads => {
-        for(let thread of threads) {
-            const newThread: {name:string, message:string } = { 
-                name: thread._id,
-                message: thread._id,
+    useEffect(() => {
+        const token = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzcyYzk0MmM0MDhjNGI5MzRmYWU1OSIsIm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjg1NTMxODEzLCJleHAiOjE3MTcwODg3Mzl9.mCre5BG5C5psHn7Eqe3W1F3IPSLam2os8Jrv7B66HDo';
+        fetch('http://localhost:5000/api/threads/allthreads', {
+            headers: {
+                'Authorization': `${token}`
             }
-            chats.push(newThread);
-            setLoading(false);
-            console.log(newThread);
-        }
-        console.log(chats[0].message);
-      });
-  }, []);
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+        })
+            .then(response => response.json())
+            .then(threads => {
+                for (let thread of threads) {
+                    const newThread: { name: string, message: string } = {
+                        name: thread._id,
+                        message: thread._id,
+                    }
+                    chats.push(newThread);
+                    setLoading(false);
+                }
+            });
+    }, []);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      {chats.map((chat, index) => (
-        <div key={index} className='chat-preview' id={chat.name}>
-          {/* <img src={chat.img} alt=''></img> */}
-          <span className='chat-preview__info'>
-            <p className='chat-preview__info__name'>{chat.name}</p>
-            <p className='chat-preview__info__message'>{chat.message}</p>
-          </span>
+    return (
+        <div>
+            {chats.map((chat, index) => (
+                <div key={index} className='chat-preview' id={chat.name} onClick={() => renderMessages(chat.name)}>
+                    {/* <img src={chat.img} alt=''></img> */}
+                    <span className='chat-preview__info'>
+                        <p className='chat-preview__info__name'>{chat.name}</p>
+                        <p className='chat-preview__info__message'>{chat.message}</p>
+                    </span>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
