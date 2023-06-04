82d144f23e9c2a26cb9eaa0b411124e4e2dd0dec
sascha@krusbersky.net <sascha.krusbersky@baramundi.com>
2023-06-04 20:44:50 +0200

    add time to bubble - thanks nicole :)

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -1,4 +1,7 @@
 import { io } from 'socket.io-client';
 
-export const socket = io();
+const socket_url: string = window.location.origin === 'http://localhost:8080'
+  ? 'http://localhost:5000'
+  : window.location.origin;
+export const socket = io(socket_url);
 
