2d9f1c87928351df4d277aa5a891b75a098196fd
SKR <sascha@krusbersky.net>
2023-06-04 15:28:37 +0200

    update cors for production

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -1,8 +1,4 @@
 import { io } from 'socket.io-client';
 
-//const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:5000';
-
-//const URL = 'http://localhost:5000';
-const URL = window.location.origin + ':5000';
-export const socket = io(URL);
+export const socket = io();
 
